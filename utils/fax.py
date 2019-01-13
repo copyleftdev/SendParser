a0add7c39e0ae323a20dbfc482be10ad7efc4017
Don Johnson <dj@codetestcode.io>
2019-01-13 08:55:32 -0800

    added abbriviations

--- utils/fax.py
+++ utils/fax.py
@@ -1,13 +1,53 @@
-
-
 class FaxUtils(object):
-
-
     def get_send_abbreviation(self):
-         abbrev = {
-             "CFR": "Confirmation To Recieve",
-             "CIG": "Calling Subscriber Identification",
-             "CRP": "Command Repeat",
-             "CSA": "Called Subscriber Internet Address"
-         }
-         return abbrev
+        abbrev = {
+            "CFR": "Confirmation To Receive",
+            "CIG": "Calling Subscriber Identification",
+            "CRP": "Command Repeat",
+            "CSA": "Called Subscriber Internet Address",
+            "CSI": "Called Subscriber Identification",
+            "CTR": "Response for Continue to Correct",
+            "DCN": "Disconnect",
+            "DCS": "Digital Command Signal",
+            "DIS": "Digital Identification Signal",
+            "DTC": "Digital Transmit Command",
+            "EOM": "End Of Message",
+            "EOP": "End of Procedure",
+            "EOR": "End of Retransmission",
+            "EOS": "End Of Selection",
+            "FCD": "Facsimile Coded Data",
+            "FCF": "Facsimile Control Field",
+            "FIF": "Facsimile Information Field",
+            "FDM": "File Diagnostics Message",
+            "FTT": "Failure To Train",
+            "HDLC": "High-level Data Link Control",
+            "JM": "Joint Menu See ITU-T Rec. V.8. F.5",
+            "MCF": "Message Confirmation",
+            "MPh": "Half Duplex Modulation Parameter See ITU-T Rec. V.34. F.3.1.4",
+            "MPS": "Multipage Signal",
+            "NSC": "Non-standard facilities Command",
+            "NSF": "Non-standard Facilities",
+            "NSS": "Non-standard Set-up",
+            "PID": "Procedure Interrupt Disconnect",
+            "PIN": "Procedure Interrupt Negative",
+            "PIP": "Procedure Interrupt Positive",
+            "PPR": "Partial Page Request",
+            "PPS": "Partial Page Signal",
+            "PRI-EOM": "Procedure Interrupt-EOM",
+            "PRI-EOP": "Procedure Interrupt-EOP",
+            "PRI-MPS": "Procedure Interrupt-MPS",
+            "PPS-NULL": "Partial Page Signal-Null",
+            "PWD": "Password(for polling)",
+            "RCP": "Return to Control for Partial page",
+            "RNR": "Receive Not Ready",
+            "RR": "Recieve Ready",
+            "RTN": "Retrain Negative",
+            "RTP": "Retrain Positive",
+            "SEP": "Selective Polling",
+            "SID": "Sender Identification",
+            "SUB": "Subaddress",
+            "TCF": "Training Check Zeros",
+            "TSI": "Transmitting Subscriber Identification",
+            "XID": "EXchange IDentification procedure",
+        }
+        return abbrev
