d0c6f695818c5cf35409499e288ea8dd39a8ab3e
Don Johnson <dj@codetestcode.io>
2019-01-28 21:28:24 -0800

    refactored  ssh client

--- utils/ssh.py
+++ utils/ssh.py
@@ -1,17 +1,31 @@
-from paramiko import SS
+from paramiko import SSHClient, AutoAddPolicy
 
 
 class SSHUtil(object):
-    def __init__(self, host, port=22, username=None, password=None):
+    def __init__(self, host, port=22, username=None, password=None, no_auth=False):
         self.host = host
         self.port = port
         self.username = username
         self.password = password
+        self.no_auth = no_auth
 
     def client(self, command):
         s = SSHClient()
-        s.load_system_host_keys()
+        if self.no_auth is True:
+            s.set_missing_host_key_policy(AutoAddPolicy())
+        else:
+            s.load_system_host_keys()
+
         s.connect(self.host, self.port)
         stdin, stdout, stderr = s.exec_command(command)
-        return {"in": stdin, "out": stdout, "err": stderr}
+        return stdout.read()
         s.close()
+
+
+# hosts = ["node-%.2d.local" % i for i in range(1, 11)]
+# for h in hosts:
+#     c = SSHUtil(host=h, no_auth=True)
+#     s = c.client(command="cat /var/log/inblogs/inblog")
+#     result = s.decode("utf-8")
+#     with open("sample_log", "a+") as lout:
+#         lout.write(result)
